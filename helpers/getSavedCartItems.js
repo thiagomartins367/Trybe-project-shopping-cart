b8c43de50cbfa0488ff48febb80d6292284f2976
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-12-02 23:13:28 -0300

    Requisito 5 feito.

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,23 +1,19 @@
 const getSavedCartItems = () => {
-
-  // seu código aqui
-  const getLocalStorage =
-    localStorage.getItem('cartItems') === null
-      ? `<li class='cart__item'></li>✄,`
-      : localStorage.getItem('cartItems');
-  const arrayCartsItemsLi = [];
-  let stringItemLi = '';
-  for (let index = 0; index < getLocalStorage.length; index+=1) {
-    if (getLocalStorage[index] === '✄') {
-      arrayCartsItemsLi.push(stringItemLi);
-      stringItemLi = '';
-      index +=1;
-    } else {
-      stringItemLi += getLocalStorage[index];
+  if (localStorage.getItem('cartItems') !== null) {
+    const getLocalStorage = localStorage.getItem('cartItems');
+    const arrayCartsItemsLi = [];
+    let stringItemLi = '';
+    for (let index = 0; index < getLocalStorage.length; index += 1) {
+      if (getLocalStorage[index] === '✄') {
+        arrayCartsItemsLi.push(stringItemLi);
+        stringItemLi = '';
+        index += 1;
+      } else {
+        stringItemLi += getLocalStorage[index];
+      }
     }
+    insertDataSaved(arrayCartsItemsLi);
   }
-  console.log('getSavedCartItems: ', arrayCartsItemsLi);
-  insertDataSaved(arrayCartsItemsLi);
 };
 
 const insertDataSaved = (arrayCartsItemsLi) => {
@@ -25,7 +21,7 @@ const insertDataSaved = (arrayCartsItemsLi) => {
   arrayCartsItemsLi.forEach(element => {
     cartItems.innerHTML += element;
   });
-}
+};
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function createProductImageElement(imageSource) {
 
 function createCustomElement(sku, element, className, innerText) {
   const e = document.createElement(element);
-  
   if (element === 'button') {
     e.className = className;
     e.classList.add(sku);
@@ -21,12 +20,10 @@ function createCustomElement(sku, element, className, innerText) {
 function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement(sku, 'span', 'item__sku', sku));
   section.appendChild(createCustomElement(sku, 'span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement(sku, 'button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
@@ -39,6 +36,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
   event.target.remove();
   saveCartItems();
+  sumSubtotal();
 }
 
 function createCartItemElement({ id:sku, title:name, price:salePrice }) {
@@ -48,6 +46,32 @@ function createCartItemElement({ id:sku, title:name, price:salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+const sumSubtotal = () => {
+  const cartItem = document.querySelectorAll('.cart__item');
+  const totalPrice = document .querySelector('.total-price');
+  let subTotal = 0;
+  let valor = false;
+  let string = '';
+  cartItem.forEach((element) => {
+    let elementString = element.outerHTML;
+    for (let index = 0; index < elementString.length; index += 1) {
+      if (elementString[index] === '$' || valor === true) {
+        if (elementString[index] === '<') {
+          break;
+        }
+        valor = true;
+        string += elementString[index];
+      }
+    }
+    string = string.replace('$', '');
+    subTotal += parseFloat(string);
+    string ='';
+    valor = false
+  });
+  subTotal = Math.round(subTotal * 100) / 100;
+  totalPrice.innerText = `${subTotal}`;
 }
 
 window.onload = () => {
@@ -63,15 +87,16 @@ window.onload = () => {
   const funcCartItems = async () => {
     const cartItems = document.querySelector('.cart__items');
     const itemButton = document.querySelectorAll('.item__add');
-
     itemButton.forEach((element) => {
       element.addEventListener('click', async (event) => {
         const itemObject = await fetchItem(event.target.classList[1]);
         cartItems.appendChild(createCartItemElement(itemObject));
         saveCartItems();
+        sumSubtotal();
       });
     });
     getSavedCartItems();
+    sumSubtotal();
     removeProductFromCart();
   };
 
